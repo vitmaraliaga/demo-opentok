ed0eaf765fe317188c61e60d1927a33f658944de
Vitmar Aliaga <cruzjhonson@gmail.com>
2017-09-04 08:36:06 -0500

    hey aoy vitmar

---
+++ js/app.js
@@ -0,0 +1,161 @@
+// Datos de inicializaciòn
+var CONFIG = {
+    apiKey: "",
+    apiSecret: "",
+    sessionId: "",
+    token: ""
+}
+
+var session;
+
+$(document).ready(function(){
+    
+    // Revisar el archivo config.js
+    if(API_KEY && TOKEN && SESSION_ID){
+        
+        CONFIG.apiKey=API_KEY;
+        CONFIG.sessionId=SESSION_ID;
+        CONFIG.token=TOKEN;
+        
+        initializeSession();
+
+    }else if(SAMPLE_SERVER_BASE_URL){
+    
+        $.get(SAMPLE_SERVER_BASE_URL + "/session", function(res){
+        
+            CONFIG.apiKey=res.apiKey;
+            CONFIG.sessionId=res.sessionId;
+            CONFIG.token=res.token;
+
+            initializeSession();
+        });
+    }
+})
+
+
+
+// Funcion de inicializaciòn
+function initializeSession(){
+    
+    session = OT.initSession(CONFIG.apiKey, CONFIG.sessionId);
+    
+    // El connect retorna una promesa
+    session.connect(CONFIG.token, function(error){
+        // Si la conexión es correcta, inicialice un editor y publique en la sesión
+        if(!error){
+            
+            //Configuraciòn basica para inciaar una publicaciòn.
+            let publisherOptions = {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%'
+            };
+            
+            // El initPublisher retorna una promesa.
+            let publisher = OT.initPublisher('div-publisher', publisherOptions, function(error){
+                if(error){
+                    console.log("Se ha producido un error al inicializar el editor:", error.name, error.message)
+                    return;
+                }
+
+                // El publisher retorna un promesa.
+                session.publish(publisher, function(error){
+                    if(error){
+                        console.log("Se ha producido un error al publicar:", error.name, error.message)
+                    }
+                });
+            });
+            
+        }else{
+            console.log("Se produjo un error al conectarse a la sesión:", error.name, error.message)
+            //handleError(error);
+        }
+    });
+
+    session.on('streamCreated', function(event){
+
+        let subscriberOptions = {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        };
+        session.subscribe(event.stream, 'div-subscriber', subscriberOptions, function(error){
+            if (error) {
+                console.log('Se ha producido un error al publicar-subscriber: ', error.name, error.message);
+              }
+        });
+    });
+
+    //evento que se lanza cuando alquien se desconecta.
+    session.on('sessionDisconnected', function(event){
+        console.log('Se ha desconectado de la sesión.', event.reason);
+    });
+
+    // recibir un mensage y agregar en el historial.
+    let msgHistory = document.querySelector("#history");
+    session.on('signal:msg', function(event){
+
+        let li = document.createElement("li");
+        li.className = event.from.connectionId === session.connection.connectionId ? 'mine' : 'theirs';
+        let div = document.createElement("div");
+        
+        // div avatar
+        let img = document.createElement("img");
+        let avatar = div;
+        avatar.className = "avatar";
+        avatar.appendChild(img);
+        
+        // div msg
+        let p = document.createElement("p");
+        p.textContent = event.data;
+        let time = document.createElement("time");
+        let hora = document.createTextNode("20:18");
+        time.appendChild(hora);
+        let msg = div;
+        msg.className = "msg";
+        msg.appendChild(p);
+        msg.appendChild(time);
+
+        // let emoji = msg.appendChild(p.appendChild(document.createElement("emoji")));
+        li.appendChild(avatar);
+        li.appendChild(msg);
+        
+
+        //let msg = document.createElement("p");
+        //msg.textContent = event.data;
+        //msg.className = event.from.connectionId === session.connection.connectionId ? 'mine' : 'theirs';
+        msgHistory.appendChild(li);
+        li.scrollIntoView();
+    });
+
+}
+//  https://codepen.io/Varo/pen/gbZzgr
+
+// Text chat
+var form = document.querySelector("form");
+var msgText = document.querySelector("#msgTxt");
+
+//Enviar una señal una vez que el usuario ingrese datos en el formulario.
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    session.signal({type: 'msg', data: msgTxt.value }, function(error){
+        if (error){
+            console.log("Error enviando la señal: ", error.name, error.message);
+        }else {
+            msgText.value = "";
+        }
+    })
+}) 
+
+
+
+
+//Manejador de errores
+function handleError(error){
+    if(error){
+        alert("ERROR: ", error.message)
+    }
+}
+
+
---
+++ js/config.js
@@ -0,0 +1,16 @@
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+var API_KEY = '45951002';
+var API_SECRET = '7fbfff187da7b0afe0b306df4dbdc9d61612de27';
+var SESSION_ID = '1_MX40NTk1MTAwMn5-MTUwNDI5MzQwNTcwMX5MQmwvSFEzM2x5UzVLcUtkdkFjaC8xQ3l-fg';
+var TOKEN = 'T1==cGFydG5lcl9pZD00NTk1MTAwMiZzaWc9NzQ5Y2JjMjFjYmQ1YmY3MGY4NmE2MjUyYmU0ZmIxNjNjNzc4OTNkNDpzZXNzaW9uX2lkPTFfTVg0ME5UazFNVEF3TW41LU1UVXdOREk1TXpRd05UY3dNWDVNUW13dlNGRXpNMng1VXpWTGNVdGtka0ZqYUM4eFEzbC1mZyZjcmVhdGVfdGltZT0xNTA0MjkzNDI2Jm5vbmNlPTAuMjYwNzE3Mzk0Mjk0NTk1NSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTA0Mjk3MDI1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
+
